418435ddb538ae8224c7ce7b9996678438fc2190
Hwee Shan <tay.hweeshan@gmail.com>
2016-05-03 14:27:37 +0800

    removing node app from this repository. I will share the front end from a separate repository.

--- dayspring-donation/config/db.js
+++
@@ -1,5 +0,0 @@
-// config/db.js
-module.exports = {
-    //url : process.env.NODE_ENV == 'development' ? 'mongodb://localhost/nric-dev' : 'mongodb://checknricuser:6l7dPFTXEzs712P@ds059692.mongolab.com:59692/nric-dev'
-    url : 'mongodb://localhost/nric-dev'
-}
